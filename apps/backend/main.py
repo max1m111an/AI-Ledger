73653602a53b58e40b46a88f13754a052cc5bcf0
max1m111an <maksimilianbegunov@inbox.ru>
2025-10-06 23:52:32 +0300

    apps/backend: make test posts for User

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from shared.models.user import User, DBUser
+from shared.database import get_session, init_db, exe_q
 
 app = FastAPI()
 
@@ -9,3 +14,28 @@ def getUser(name: str):
         "status": "200",
         "name": name,
     }
+
+
+@app.on_event("startup")
+async def initDb():
+    await init_db()
+
+
+@app.post("/add")
+async def addUser(new_user: User, session: AsyncSession = Depends(get_session)):
+    dbuser: DBUser = DBUser(name=new_user.login, password=new_user.password)
+    session.add(dbuser)
+    await session.commit()
+    await session.refresh(dbuser)
+    return dbuser
+
+@app.post("/id")
+async def getUserByID(user_id: int, session: AsyncSession = Depends(get_session)):
+    result = await exe_q(select(DBUser).where(DBUser.id == user_id), session, True)
+    return result
+
+
+@app.post("/")
+async def getUsers(session: AsyncSession = Depends(get_session)):
+    result = await exe_q(select(DBUser), session)
+    return result
