d796bd5fccbfffad10d73cbcc0b4cbe5e7941abd
max1m111an <maksimilianbegunov@inbox.ru>
2025-10-06 23:52:49 +0300

    shared/models/user: add DBUser

--- shared/models/user.py
+++ shared/models/user.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
 
 
 class User(BaseModel):
     login: str
     password: str
+
+
+class DBUser(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: int = Field(primary_key=True)
+    name: str = Field(nullable=False)
+    password: str = Field(nullable=False)
