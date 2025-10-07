a4d32290375c8dae6ad9d309a7a5501735a9471a
qurlya <pingvining05@mail.ru>
2025-10-07 21:30:55 +0300

    frontend: add config

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [ react() ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+            "@interfaces": path.resolve(__dirname, "./src/interfaces"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: "@use \"@assets/scss/consts\" as *;",
+            },
+        },
+    },
 });
