90c53b33886fde163ba20d3fd71248d6b679f2e8
Группа 4316 Александрова А.С. <na.aleksandrova2014@mail.ru>
2025-10-18 04:54:04 +0300

    frontend: add icons

--- apps/frontend/src/layouts/PublicLayout.tsx
+++ apps/frontend/src/layouts/PublicLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../widgets/Sidebar.tsx";
 import { Header } from "@/widgets/Header.tsx";
+import sprite from "@/assets/scss/components/Icons.svg?raw";
 
 export default function PublicLayout() {
     return (
         <div className="app-container">
+            <div style={ { display: "none" } } dangerouslySetInnerHTML={ { __html: sprite } } />
             <Sidebar />
             <Header />
             <main className="main-content">
