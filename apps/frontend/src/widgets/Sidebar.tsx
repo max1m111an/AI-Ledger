0756be0751157b01eb39d411ee4b79a552feabc6
Группа 4316 Александрова А.С. <na.aleksandrova2014@mail.ru>
2025-10-18 04:54:04 +0300

    frontend: add icons

--- apps/frontend/src/widgets/Sidebar.tsx
+++ apps/frontend/src/widgets/Sidebar.tsx
@@ -12,57 +12,40 @@ export const Sidebar: React.FC = () => {
             </div>
             <nav className="sidebar_nav_div">
                 <NavLink to={ ROUTES.MAIN } className="Button nav">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <rect x="3" y="3" width="7" height="7"></rect>
-                        <rect x="14" y="3" width="7" height="7"></rect>
-                        <rect x="14" y="14" width="7" height="7"></rect>
-                        <rect x="3" y="14" width="7" height="7"></rect>
+                    <svg width="20" height="20" className="Icon">
+                        <use href="#Main" />
                     </svg>
                     Главная
                 </NavLink>
                 <NavLink to={ ROUTES.OPERATION } className="Button nav">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <line x1="8" y1="6" x2="21" y2="6"></line>
-                        <line x1="8" y1="12" x2="21" y2="12"></line>
-                        <line x1="8" y1="18" x2="21" y2="18"></line>
-                        <line x1="3" y1="6" x2="3.01" y2="6"></line>
-                        <line x1="3" y1="12" x2="3.01" y2="12"></line>
-                        <line x1="3" y1="18" x2="3.01" y2="18"></line>
+                    <svg width="20" height="20" className="Icon">
+                        <use href="#Operations" />
                     </svg>
                     Операции
                 </NavLink>
                 <NavLink to={ ROUTES.UPLOADING_RECEIPTS } className="Button nav">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                        <polyline points="17 8 12 3 7 8"></polyline>
-                        <line x1="12" y1="3" x2="12" y2="15"></line>
-                        <path d="m16 16-4-4-4 4"></path>
+                    <svg width="20" height="20" className="Icon">
+                        <use href="#UploadingReceipts" />
                     </svg>
                     Загрузка чеков
                 </NavLink>
                 <NavLink to={ ROUTES.SUBSCRIPTIONS } className="Button nav">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="m17 2 4 4-4 4"></path>
-                        <path d="M3 11v-1a4 4 0 0 1 4-4h14"></path>
-                        <path d="m7 22-4-4 4-4"></path>
-                        <path d="M21 13v1a4 4 0 0 1-4 4H3"></path>
+                    <svg width="20" height="20" className="Icon">
+                        <use href="#Subscriptions" />
                     </svg>
                     Подписки
                 </NavLink>
                 <NavLink to={ ROUTES.ANALYTICS } className="Button nav">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <line x1="18" y1="20" x2="18" y2="10"></line>
-                        <line x1="12" y1="20" x2="12" y2="4"></line>
-                        <line x1="6" y1="20" x2="6" y2="14"></line>
+                    <svg width="20" height="20" className="Icon">
+                        <use href="#Analytics" />
                     </svg>
                     Аналитика
                 </NavLink>
             </nav>
             <div className="sidebar_user_div">
                 <NavLink to={ ROUTES.PROFILE } className="Button nav">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <circle cx="12" cy="12" r="3"></circle>
-                        <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
+                    <svg width="20" height="20" className="Icon">
+                        <use href="#Settings" />
                     </svg>
                     Настройки и профиль
                 </NavLink>
