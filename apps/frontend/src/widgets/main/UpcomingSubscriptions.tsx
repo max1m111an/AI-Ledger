1a4265613db1a88c1e1dc19fed7f06550b54eec0
Qurlya <pingvining05@mail.ru>
2025-10-30 20:01:16 +0300

    frontend/UpcomingSubscriptions: add widget Writedown

--- apps/frontend/src/widgets/main/UpcomingSubscriptions.tsx
+++ apps/frontend/src/widgets/main/UpcomingSubscriptions.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "@assets/scss/index.scss";
+import { NavLink } from "react-router-dom";
+import { ROUTES } from "@/configs/RoutesConst.ts";
 
 
 interface LastWritedownsProps {
@@ -7,8 +9,46 @@ interface LastWritedownsProps {
 }
 
 export default function UpcomingSubscriptions({ className }: LastWritedownsProps) {
+    const writeDowns = [
+        {
+            name: "Youtube Premium",
+            date: "через 2 дня",
+            value: -320,
+        },
+        {
+            name: "Adobe",
+            date: "через 5 дней",
+            value: -320,
+        }, {
+            name: "Донаты Паше <3",
+            date: "forever",
+            value: -666,
+        },
+
+    ];
+
     return (
         <div className={ className }>
+            <div className="upcomingSubscriptions title_div">
+                <p className="text title">Списания</p>
+                <p className="text subaddition">(7 дней)</p>
+                <NavLink to={ ROUTES.SUBSCRIPTIONS } className="text links">Все</NavLink>
+            </div>
+            <div className="upcomingSubscriptions upcomingSubscription_list">
+                {writeDowns.map((writeDown, index) => (
+                    <div key={ index } className="upcomingSubscriptions upcomingSubscription_div">
+                        <svg className="icon magic_mint">
+                            <use href="#Subscriptions" />
+                        </svg>
+                        <div className="today subscription_info_div">
+                            <span className="text">{writeDown.name}</span>
+                            <span className="text addition_2">{writeDown.date}</span>
+                        </div>
+                        <p className="text">{writeDown.value}</p>
+                    </div>
+                ))}
+                <button className="Button uploadReceipt">Загрузить чек</button>
+            </div>
         </div>
     );
 }
