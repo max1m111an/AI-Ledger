4e08b8e98019cc692f9789bdae20e20622bfee6e
qurlya <pingvining05@mail.ru>
2025-10-30 12:58:55 +0300

    frontend/TodayStatistics: continue Subscription - add Subscription - add Icon

--- apps/frontend/src/widgets/main/todayStatistics/Subscription.tsx
+++ apps/frontend/src/widgets/main/todayStatistics/Subscription.tsx
@@ -1,22 +1,51 @@
 import React from "react";
 import "@assets/scss/index.scss";
+import {
+    NavLink,
+} from "react-router-dom";
+import { ROUTES } from "@/configs/RoutesConst.ts";
 
 
 export default function Subscription() {
+    const subscriptions = [
+        {
+            name: "Яндекс музыка",
+            period: "Ежемесячно",
+        },
+        {
+            name: "Spotify",
+            period: "Ежемесячно",
+        },
+        {
+            name: "Youtube",
+            period: "Ежемесячно",
+        },
+    ];
+
     return (
         <div className="today subscription_div_1">
-            <p className="text title">Сегодняшние напоминания</p>
+            <div className="today title_div">
+                <p className="text title">
+                    <svg className="icon">
+                        <use href="#Reminder" />
+                    </svg>
+                    Сегодняшние напоминания
+                </p>
+                <NavLink to={ ROUTES.SUBSCRIPTIONS } className="text links">К подпискам</NavLink>
+            </div>
             <div className="today subscription_div_2">
-                <div className="today subscription_div_3">
-                    <p>Яндекс музыка</p>
-                </div>
-                <div className="today subscription_div_3">
-                    <p>Яндекс музыка</p>
-
-                </div><div className="today subscription_div_3">
-                    <p>Яндекс музыка</p>
-
-                </div>
+                {subscriptions.map((subscription, index) => (
+                    <div key={ index } className="today subscription_div_3">
+                        <svg className="icon magic_mint">
+                            <use href="#Subscriptions" />
+                        </svg>
+                        <div className="today subscription_info_div">
+                            <span className="text">{subscription.name}</span>
+                            <span className="text addition_2">{subscription.period}</span>
+                        </div>
+                        <p className="text">- 299</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
